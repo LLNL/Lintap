1ba705de691cec9a182123fdcbbaeec5cecce72d
Grant Johnson <grantj@llnl.gov>
2024-08-23 15:52:44 +0000

    Added arguments for SCAP path and naming.

--- full-capture.sh
+++ full-capture.sh
@@ -1,3 +1,3 @@
 # Run with rotating files, foraker filter, minimal snaplen
 # Writes SCAP and executes chisel, which writes CSVs
-sysdig -c ./process_events.lua -zw data/acme-alpha1.scap -s 8 -C 50 -F "((evt.type=execve and evt.dir=<) or (evt.type=clone and evt.dir=>) or (evt.type=vfork and evt.dir=<) or evt.type=procexit) or (fd.type=file and (evt.type=open or evt.type=read or evt.type=write or evt.type=close)) or (fd.l4proto=tcp or fd.l4proto=udp)" 
+sysdig -c ./process_events.lua -zw $1/acme-alpha$2.scap -s 8 -C 50 -F "((evt.type=execve and evt.dir=<) or (evt.type=clone and evt.dir=>) or (evt.type=vfork and evt.dir=<) or evt.type=procexit) or (fd.type=file and (evt.type=open or evt.type=read or evt.type=write or evt.type=close)) or (fd.l4proto=tcp or fd.l4proto=udp)" 

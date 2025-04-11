32fc7d8c74d088bcad158f4f54a5ce12553a98c6
johnson30 <grantj@llnl.gov>
2025-04-11 15:59:47 -0700

    Fixed: had an extra paren on the process filter

--- full-capture.sh
+++ full-capture.sh
@@ -16,7 +16,7 @@ echo "Writing SCAP files to:   $scapfile"
 echo "Writing Lintap files to: $lintappath"
 
 # Filters for events
-process_filter="((evt.type=execve and evt.dir=<) or (evt.type=clone and evt.dir=>) or (evt.type=vfork and evt.dir=<) or evt.type=procexit))" 
+process_filter="((evt.type=execve and evt.dir=<) or (evt.type=clone and evt.dir=>) or (evt.type=vfork and evt.dir=<) or evt.type=procexit)" 
 file_filter="fd.type=file and (evt.type=openat or evt.type=open or evt.type=read or evt.type=write or evt.type=close)"
 network_filter="fd.l4proto=tcp or fd.l4proto=udp"
 

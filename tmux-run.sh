3dec316a79004825ef5e445ea4f62609957230b0
Grant Johnson <grantj@llnl.gov>
2026-05-21 11:18:17 -0700

    Make Lintap tmux run paths configurable for pidstat collection

--- tmux-run.sh
+++ tmux-run.sh
@@ -2,10 +2,12 @@
 
 # Run both the Lintap sensor and the OS performance data collector (pidstat-collect) in parallel, in foreground sessions.
 
-WINTAP_HOME="~/git/wintap/wintap"
+WINTAP_HOME="${WINTAP_HOME:-$HOME/git/wintap/wintap}"
+WINTAP_DATA_ROOT="${WINTAP_DATA_ROOT:-/home/ubuntu/data/debug}"
+PIDSTAT_OUTPUT_PATH="${PIDSTAT_OUTPUT_PATH:-$WINTAP_DATA_ROOT/pidstat}"
 
 # Name of the tmux session
-SESSION="lintap_dev"
+SESSION="${SESSION:-lintap_dev}"
 
 # Kill existing session if it exists
 tmux kill-session -t $SESSION 2>/dev/null
@@ -16,11 +18,11 @@ tmux new-session -d -s $SESSION
 # Split the window into two panes
 tmux split-window -v -t $SESSION
 
-# Pane 1: Run the .NET project (sudo required, from the other projects dir)
-tmux send-keys -t $SESSION:0.0 "sudo WINTAP_DATA_ROOT=/home/ubuntu/data/debug dotnet run --project $WINTAP_HOME/Lintap.csproj" C-m
+# Pane 1: Run the .NET project. Preserve WINTAP_DATA_ROOT through sudo.
+tmux send-keys -t "$SESSION:0.0" "sudo WINTAP_DATA_ROOT='$WINTAP_DATA_ROOT' dotnet run --project '$WINTAP_HOME/Lintap.csproj'" C-m
 
-# Pane 2: Run the collector
-tmux send-keys -t $SESSION:0.1 "./pidstat-collect.sh" C-m
+# Pane 2: Run the collector into the same run root.
+tmux send-keys -t "$SESSION:0.1" "./pidstat-collect.sh '$PIDSTAT_OUTPUT_PATH'" C-m
 
 # Attach to the session
 tmux attach-session -t $SESSION
